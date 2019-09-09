2112fd55164b2363dfc61dc9d712ca9593afac35
Buntybru <buntybru@gmail.com>
2019-09-09 17:28:16 +0530

    First Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MaincompComponent } from './maincomp/maincomp.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path:'',
+    component:LoginComponent
+  },
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  
+  {
+    path:'main',
+    component:MaincompComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MaincompComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
